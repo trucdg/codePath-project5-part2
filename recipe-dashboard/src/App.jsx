96b78fa047d6c030be6767b1ac5699c64834241e
Truc Duong <trucdg@bu.edu>
2023-03-28 23:54:41 -0400

    add FontAwesome, add icons and some stylings

--- recipe-dashboard/src/App.jsx
+++ recipe-dashboard/src/App.jsx
@@ -3,8 +3,26 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello </h1>
+    <div className="row">
+      <div className="col left">
+        <h2>
+          <i className="fa-solid fa-utensils"></i>RecipesBoard
+        </h2>
+        <ul>
+          <li>
+            <i className="fa-solid fa-house"></i>Dashboard
+          </li>
+          <li>
+            <i className="fa-solid fa-magnifying-glass"></i>Search
+          </li>
+          <li>
+            <i className="fa-solid fa-circle-info"></i>About
+          </li>
+        </ul>
+      </div>
+      <div className="col right">
+        <h1> Hello </h1>
+      </div>
     </div>
   );
 }
