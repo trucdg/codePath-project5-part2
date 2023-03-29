19721b297137ebc23bfd44d07cf3cb9d5b25bd0a
Truc Duong <trucdg@bu.edu>
2023-03-29 01:31:30 -0400

    fetch data from the spoonacular API and pass as props to the FeaturedRecipe component

--- recipe-dashboard/src/App.jsx
+++ recipe-dashboard/src/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FeaturedRecipe from "./component/FeaturedRecipe";
 import RecipeTable from "./component/RecipeTable";
 import "./App.css";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
+  const dummyRecipe = {
+    title: "No recipe found",
+    image: "",
+    vegan: false,
+  };
+  const [recipesList, setRecipesList] = useState(null);
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=20`
+      );
+      const json = await response.json();
+      setRecipesList(json);
+      console.log(json);
+    };
+    fetchRecipes().catch(console.error);
+  }, []);
   return (
     <div className="row">
-      <div className="col left">
+      <div className="left">
         <h2>
           <i className="fa-solid fa-utensils"></i>RecipesBoard
         </h2>
@@ -22,9 +40,11 @@ function App() {
           </li>
         </ul>
       </div>
-      <div className="col right">
-        <FeaturedRecipe />
-        <div className="break"></div>
+      <div className="right">
+        <FeaturedRecipe
+          recipe={recipesList ? recipesList.results[0] : dummyRecipe}
+        />
+        {/* <div className="break"></div> */}
         <RecipeTable />
       </div>
     </div>
--- recipe-dashboard/src/component/FeaturedRecipe.jsx
+++ recipe-dashboard/src/component/FeaturedRecipe.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 
-const FeaturedRecipe = () => {
-  const recipe = ["name", "image", "diet"];
+const FeaturedRecipe = ({ recipe }) => {
+  console.log(recipe);
+  console.log(recipe.title);
+  console.log(recipe.image);
+  const recipes = ["name", "image", "diet"];
   return (
-    <div>
+    <div className="featured-cont">
       <h2>Featured Recipe</h2>
       <ul className="d-flex">
         <li className="card">
-          <h4>Name</h4>
-          <p>recipe name</p>
+          <h4>Recipe Name</h4>
+          <p>
+            <a href="recipe.sourceUrl">{recipe.title}</a>
+          </p>
+        </li>
+        <li className="card">
+          <h4>Image</h4>
+          <img src={recipe.image} className="featured-img" />
         </li>
         <li className="card">
           <h4>Diet types</h4>
-          <p>healthy</p>
+          <ul>
+            {recipe.diets.map((diet) => (
+              <li>{diet}</li>
+            ))}
+          </ul>
         </li>
         <li className="card">
-          <h4>vegan</h4>
+          <h4>Ready In</h4>
+          <h3>{recipe.readyInMinutes} minutes</h3>
         </li>
       </ul>
     </div>
